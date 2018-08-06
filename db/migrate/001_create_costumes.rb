e3736a75a3639a10463047e4b2d30c00cfa9c2fa
Jonathan Luk <jluk87@gmail.com>
2018-08-06 17:12:31 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -5,7 +5,7 @@
 # 002_create_costumes.rb becomes class CreateCostumes
 
 # Define a change method in which to do the migration
-# In this change method, create columns with the correct names and 
+# In this change method, create columns with the correct names and
 # value types according to the spec
 
 class CreateCostumes < Active::Migration[5.1]
@@ -17,6 +17,6 @@ class CreateCostumes < Active::Migration[5.1]
       t.string :image_url
     end
   end
-  
+
 
 end
--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,16 @@
 # Create your costume_stores migration here
+class CreateCostumeStores < Active::Migration[5.1]
+  def change
+    create_table :costumestores do |t|
+      t.string :name
+      t.string :location
+      t.integer :number_of_costumes
+      t.integer :number_of_employees
+      t.boolean :in_business
+      t.time :opening_time
+      t.time :closing_time
+    end
+  end
+
+
+end
